d526bcf46c6e9d72e05e3bc8ce0596ec376c9fb8
hsidd1 <hamzasiddiqui247@gmail.com>
2024-01-31 18:25:52 -0500

    setting up lab 2

---
+++ Max-Heaps-Young-Tableaux/src/heapyt/MaxBinHeap.java
@@ -0,0 +1,7 @@
+package heapyt;
+
+public class MaxBinHeap {
+    private int[] heap; // array to store the keys of the heap
+    private int size; // number of elements in the heap
+    
+}
---
+++ Max-Heaps-Young-Tableaux/src/heapyt/YoungT.java
@@ -0,0 +1,8 @@
+package heapyt;
+
+public class YoungT {
+    private int[][] youngTableau; // 2D array to store the integers of the Young tableau
+    private int numFiniteInts; // number of finite integers in the Young tableau
+    private static final int INFINITY = Integer.MAX_VALUE; // infinity value to be used in the Young tableau
+
+}
